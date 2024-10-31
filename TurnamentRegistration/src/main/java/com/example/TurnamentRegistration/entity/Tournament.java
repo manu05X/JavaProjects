85dc675a4c0e762dd85e5571800d3f7ba441b4d2
Manish Kumar <k.manu00005@gmail.com>
2024-10-31 16:40:33 +0530

    Adding application.properties and adding OneToMany and JoinColumn in Tournaments

--- TurnamentRegistration/src/main/java/com/example/TurnamentRegistration/entity/Tournament.java
+++ TurnamentRegistration/src/main/java/com/example/TurnamentRegistration/entity/Tournament.java
@@ -1,20 +1,15 @@
 package com.example.TurnamentRegistration.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+
 @Entity
 public class Tournament {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -22,23 +17,61 @@ public class Tournament {
     private String name;
     private String location;
 
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(name="tournament_id")
     private List<Registration> registrations = new ArrayList<>();
 
-    //update constructor, generate getter and setter methods , update toString()
-    public Tournament(String name, String location, List<Registration> registrations){
+    public Tournament() {
+    }
+
+    public Tournament(String name, String location) {
+        super();
+        this.name = name;
+        this.location = location;
+    }
+
+    public Tournament(String name, String location, List<Registration> registrations) {
         super();
         this.name = name;
         this.location = location;
         this.registrations = registrations;
     }
 
+    public int getId() {
+        return id;
+    }
 
-    public List<Registration> getRegistrations(){
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Registration> getRegistrations() {
         return registrations;
     }
 
-    public void setRegistrations(List<Registration> registrations){
+    public void setRegistrations(List<Registration> registrations) {
         this.registrations = registrations;
+    }
+
+    //set up one to many relationship
+    public void addRegistration(Registration reg) {
+        registrations.add(reg);
     }
 
     @Override
@@ -46,5 +79,4 @@ public class Tournament {
         return "Tournament [id=" + id + ", name=" + name + ", location=" + location + ", registrations=" + registrations
                 + "]";
     }
-
 }
