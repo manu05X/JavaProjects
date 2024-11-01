f05829f3e12001d3d7dc5ae05b5c5ec3b27d3ef0
Manish Kumar <k.manu00005@gmail.com>
2024-11-01 19:13:55 +0530

    Initializing Book Project

---
+++ BookREST/src/test/java/com/example/BookREST/BookRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
