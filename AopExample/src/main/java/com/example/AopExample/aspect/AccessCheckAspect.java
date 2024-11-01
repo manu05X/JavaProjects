efbbcb8e527205bd096bd84158998d6f235f6452
Manish Kumar <k.manu00005@gmail.com>
2024-11-01 18:04:01 +0530

    Defining After Aspect

--- AopExample/src/main/java/com/example/AopExample/aspect/AccessCheckAspect.java
+++ AopExample/src/main/java/com/example/AopExample/aspect/AccessCheckAspect.java
@@ -11,7 +11,11 @@ import org.slf4j.LoggerFactory;
 public class AccessCheckAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
+    @Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..)) || execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+    //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(String))")
+    //@Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..))")
+    //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+    //@Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
     public void before(JoinPoint joinPoint) {
         //	logger.info("Intercepted method call");
         logger.info("Intercepted call before execution of: {}", joinPoint);
@@ -24,4 +28,17 @@ public class AccessCheckAspect {
 1. Define aspect class.
 2. Write methods containing the advice to be executed when method calls are intercepted.
 3. Write pointcut expressions for intercepting method calls.
+
+_______________________________________________________________________________________________
+
+Intercepting all method calls in a package : @Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
+Intercepting all method calls :              @Before("execution(* io.datajek.springaop.movierecommenderaop..*.*(..))")
+Intercepting calls using return type :       @Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+Intercepting calls to a specific method :    @Before("execution(String io.datajek.springaop.movierecommenderaop..*.*Filtering(..))")
+Intercepting calls with specific method arguments :
+                                             @Before("execution(* io.datajek.springaop.movierecommenderaop..*.*(String))")
+                                             @Before("execution(*io.datajek.springaop.movierecommenderaop..*.*(String,..))")
+Combining pointcut expressions :
+                                             @Before ("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..))
+                                                    || execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
 * */
