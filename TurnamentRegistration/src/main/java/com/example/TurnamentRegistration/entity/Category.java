90d041abb6bc9dfab8cbf032cbaf51e2bad95b3e
Manish Kumar <k.manu00005@gmail.com>
2024-11-01 12:15:06 +0530

    Adding category for Many-to-Many bidirectional relation

--- TurnamentRegistration/src/main/java/com/example/TurnamentRegistration/entity/Category.java
+++ TurnamentRegistration/src/main/java/com/example/TurnamentRegistration/entity/Category.java
@@ -1,6 +1,10 @@
 package com.example.TurnamentRegistration.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -11,28 +15,52 @@ public class Category {
     @Column(unique = true)
     private String name;
 
+    @ManyToMany(mappedBy= "playingCategories",
+            cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch=FetchType.LAZY)
+    @JsonIgnoreProperties("playingCategories")
+    private List<Tournament> tournaments = new ArrayList<>();
+
     public Category(){
 
     }
 
     public int getId(){
+
         return id;
     }
 
     public void setId(int id){
+
         this.id = id;
     }
 
     public String getName(){
+
         return name;
     }
 
     public void setName(String name){
+
         this.name = name;
     }
 
+    public List<Tournament> getTournaments(){
+        return tournaments;
+    }
+
+    public void setTournaments(List<Tournament> tournaments){
+        this.tournaments = tournaments;
+    }
+
+    @PreRemove
+    private void removeCategoriesFromTournaments() {
+        for (Tournament tournament : tournaments) {
+            tournament.getPlayingCategories().remove(this);
+        }
+    }
+
     @Override
-    public String   toString() {
-        return "Category [id=" + id + ", name=" + name + "]";
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", tournaments=" + tournaments + "]";
     }
 }
