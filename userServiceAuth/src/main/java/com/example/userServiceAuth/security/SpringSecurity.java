e59bb37c06461547ee4c8a1888c0bd1b83f2f109
Manish Kumar <k.manu00005@gmail.com>
2025-01-24 14:27:44 +0530

    Adding OAuth2 to project

--- userServiceAuth/src/main/java/com/example/userServiceAuth/security/SpringSecurity.java
+++ userServiceAuth/src/main/java/com/example/userServiceAuth/security/SpringSecurity.java
@@ -9,15 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurity {
 
-    @Bean
-    public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
-        http.cors().disable();
-        http.csrf().disable();
-
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        return http.build();
-
-    }
+//    @Bean
+//    public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+//        http.cors().disable();
+//        http.csrf().disable();
+//
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+//        return http.build();
+//
+//    }
 
     // Not to create a new instance each time it is used we will keep it in SpringSecurity and make its bean their,
     // so we do not need to create a instance each time only single instance of it will be present across the app.
